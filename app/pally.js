859280978ad74f95d27a9be7cdde0172c463ebfc
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-14 16:04:22 +0100

    added my 18th test and make sure it passed by modifying the function

--- app/pally.js
+++ app/pally.js
@@ -1,18 +1,17 @@
 'use strict'
 const pally = {
-	pali(str) {
+	reverseString(str) {
 		if(typeof str !== "string") {
 			const msg = "Invalid Input, Expecting a string!"
 			return msg;
 		}
+		else if(str === ""){
+			return null;
+		}
 		let str_split  = str.toLowerCase();
 		str_split = str_split.replace(/[\W_]/g,"");
 		const final = str_split.split("").reverse().join("");
-		if(str_split === final){
-			return true;
-		} else {
-			return str_split;
-		}
+		return (str_split === final)? true: final;
 }
 }
 module.exports = pally;
--- spec/pally-spec.js
+++ spec/pally-spec.js
@@ -1,53 +1,95 @@
   'use strict'
 const app = require('../app/pally.js');
 
-    describe('Palindrome', function() {
+describe('Palindrome', function() {
 
   it('bob should return true', function() {
-    expect(app.pali("bob")).toBeTruthy();
+    expect(app.reverseString("bob")).toBeTruthy();
   });
 
   it('bob should return true', function() {
-    expect(app.pali("b==o==b")).toBeTruthy();
+    expect(app.reverseString("b==o==b")).toBeTruthy();
   });
 
    it('typeof input should return string', function() {
-    expect(typeof app.pali("mother")).toEqual("string");
+    expect(typeof app.reverseString("mother")).toEqual("string");
   });
 
    it('123321 should be truthy', function() {
-    expect(app.pali("123321")).toBeTruthy();
+    expect(app.reverseString("123321")).toBeTruthy();
+  });
+
+   it('should return invalid input for unAnticipated Inputs', function() {
+    expect(app.reverseString("{[1237&556]}")).toEqual('6557321');
   });
 
    it('should return invalid input for unAnticipated Inputs', function() {
    	const expectedCount = "Invalid Input, Expecting a string!";
-    expect(app.pali("{[1237&556]}")).toEqual('1237556');
+    expect(app.reverseString(true)).toEqual(expectedCount);
   });
 
    it('should return invalid input for unAnticipated Inputs', function() {
    	const expectedCount = "Invalid Input, Expecting a string!";
-    expect(app.pali(true)).toEqual(expectedCount);
-  });
-
-   it('should return invalid input for unAnticipated Inputs', function() {
-   	const expectedCount = "Invalid Input, Expecting a string!";
-    expect(app.pali(1233)).toEqual(expectedCount);
+    expect(app.reverseString(1233)).toEqual(expectedCount);
   });
 
    it('2+2=2+2 should be truthy', function() {
-    expect(app.pali("2+2=2+2")).toBeTruthy();
+    expect(app.reverseString("2+2=2+2")).toBeTruthy();
   });
 
  it('<[o_o]> should be truthy', function() {
-    expect(app.pali("<[o_o]>")).toBeTruthy();
+    expect(app.reverseString("<[o_o]>")).toBeTruthy();
   });
 
   it('mama  ---  mama //  mama should be truthy', function() {
-    expect(app.pali("mama  ---  mama //  mama")).toBeTruthy();
+    expect(app.reverseString("mama  ---  mama //  mama")).toBeTruthy();
   });
 
 	it('bobbqwert should return bobbqwert', function() {
-    expect(app.pali("bobbqwert")).toBe("bobbqwert");
+    expect(app.reverseString("bobbqwert")).toBe('trewqbbob');
   });
+});
+
+describe("Produce the reverse order of a word: ", function() {
+  describe("Case for en empty string", function() {
+
+    it("should return null for empty string", function() {
+      expect(app.reverseString('')).toEqual(null);
+    });
+
+  });
+
+  describe("Case for palindromes", function() {
+
+    it("should return true for `anna`", function() {
+      expect(app.reverseString('anna')).toEqual(true);
+    });
+
+    it("should return true for `NaN`", function() {
+      expect(app.reverseString('NaN')).toEqual(true);
+    });
+
+    it("should return true for `civic`", function() {
+      expect(app.reverseString('civic')).toEqual(true);
+    });
+
+  });
+
+  describe("Case for normal words", function() {
+
+    it("should return `skoob` for `books`", function() {
+      expect(app.reverseString('books')).toEqual('skoob');
+    });
+
+    it("should return `nomolos` for `solomon`", function() {
+      expect(app.reverseString('solomon')).toEqual('nomolos');
+    });
+
+    it("should return `csim` for `misc`", function() {
+      expect(app.reverseString('misc')).toEqual('csim');
+    });
+
+  });
+
 });
   
