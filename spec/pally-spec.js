b3ea663f50a39222beb1d8a6e48f9fb840761619
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-14 15:13:31 +0100

    i added a new test and make sure it passed

--- spec/pally-spec.js
+++ spec/pally-spec.js
@@ -7,5 +7,17 @@ const app = require('../app/pally.js');
     expect(app.pali("bob")).toBeTruthy();
   });
 
+  it('bob should return true', function() {
+    expect(app.pali("b==o==b")).toBeTruthy();
+  });
+
+   it('bob should return mother', function() {
+   	const expectedCount = "mother";
+    expect(app.pali("mother")).toEqual(expectedCount);
+  });
+
+   it('bob should return mother', function() {
+    expect(app.pali("123321")).toBeTruthy();
+  });
 });
   
